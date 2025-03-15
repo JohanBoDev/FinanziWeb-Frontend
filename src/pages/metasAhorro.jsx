ba65523a3aea808c73651585ea533919d837210f
JohanBoDev <bohorquezjohan958@gmail.com>
2025-03-15 18:30:42 -0500

    Se le cambió el fondo a la page metas de ahorros

--- src/pages/metasAhorro.jsx
+++ src/pages/metasAhorro.jsx
@@ -2,7 +2,7 @@ import '../Styles/calculadora.css'
 
 const MetasAhorro = () => {
   return (
-    <div className="bg-ahorros h-screen py-10 flex flex-col  justify-center text-white text-2xl">
+    <div className="bg-conversor h-screen py-10 flex flex-col  justify-center text-white text-2xl">
         <h1 className="text-gray-200 text-8xl mt-5 text-center tracking-wider uppercase drop-shadow-lg">
           Metas de ahorro
         </h1>    </div>
